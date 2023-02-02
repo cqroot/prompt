7a7416786c5c05a8140b0ef98b327923d09e99dc
cqroot <cqroot@outlook.com>
2023-02-02 21:03:09 +0800

    feat: set a width on the help menu

--- model.go
+++ model.go
@@ -20,6 +20,8 @@ func (m Prompt) Init() tea.Cmd {
 
 func (m Prompt) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.help.Width = msg.Width
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
