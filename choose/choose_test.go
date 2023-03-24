c233416cbceb004b799fc559ccfa5c337b2e4982
Keith Chu <cqroot@outlook.com>
2023-03-24 19:03:02 +0800

    test: appearance tests moved to themes_test

--- choose/choose_test.go
+++ choose/choose_test.go
@@ -1,119 +1,61 @@
 package choose_test
 
 import (
+	"bytes"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/cqroot/prompt/choose"
-	"github.com/cqroot/prompt/constants"
 	"github.com/cqroot/prompt/tester"
+	"github.com/stretchr/testify/require"
 )
 
-type Testcase struct {
-	model choose.Model
-	keys  []byte
-	data  string
-	view  string
-}
-
-func testcases() []Testcase {
+func TestChoose(t *testing.T) {
 	items := []string{"Item 1", "Item 2", "Item 3"}
 
-	// Vertical Test Cases
-	vTcs := []struct {
-		keys []byte
-		data string
+	for _, testcase := range []struct {
+		model choose.Model
+		keys  []byte
+		data  string
 	}{
-		{keys: []byte("\r\n"), data: "Item 1"},
-		{keys: []byte("kkjjj\r\n"), data: "Item 2"},
-		{keys: []byte{'k', 'k', tester.KeyTab, tester.KeyTab, tester.KeyTab, '\r', '\n'}, data: "Item 2"},
-	}
-
-	// Horizontal Test Cases
-	hTcs := []struct {
-		keys []byte
-		data string
-	}{
-		{keys: []byte("\r\n"), data: "Item 1"},
-		{keys: []byte("hhlll\r\n"), data: "Item 2"},
-		{keys: []byte{'h', 'h', tester.KeyTab, tester.KeyTab, tester.KeyTab, '\r', '\n'}, data: "Item 2"},
-	}
-
-	testcases := make([]Testcase, 0, 20)
-
-	for _, vTc := range vTcs {
-		testcases = append(testcases, Testcase{
+		{
 			model: *choose.NewWithStrings(items),
-			view:  "\n• Item 1\n  Item 2\n  Item 3\n",
-			keys:  vTc.keys,
-			data:  vTc.data,
-		})
-		testcases = append(testcases, Testcase{
-			model: *choose.NewWithStrings(items, choose.WithHelp(true)),
-			view:  "\n• Item 1\n  Item 2\n  Item 3\n\n? toggle help • enter confirm • q quit",
-			keys:  vTc.keys,
-			data:  vTc.data,
-		})
+			keys:  []byte("\r\n"),
+			data:  "Item 1",
+		},
+		{
+			model: *choose.NewWithStrings(items),
+			keys:  []byte("kkjjj\r\n"),
+			data:  "Item 2",
+		},
+		{
+			model: *choose.NewWithStrings(items),
+			keys:  []byte{'k', 'k', tester.KeyTab, tester.KeyTab, tester.KeyTab, '\r', '\n'},
+			data:  "Item 2",
+		},
+		{
+			model: *choose.NewWithStrings(items, choose.WithKeyMap(choose.HorizontalKeyMap)),
+			keys:  []byte("\r\n"),
+			data:  "Item 1",
+		},
+		{
+			model: *choose.NewWithStrings(items, choose.WithKeyMap(choose.HorizontalKeyMap)),
+			keys:  []byte("hhlll\r\n"),
+			data:  "Item 2",
+		},
+		{
+			model: *choose.NewWithStrings(items, choose.WithKeyMap(choose.HorizontalKeyMap)),
+			keys:  []byte{'h', 'h', tester.KeyTab, tester.KeyTab, tester.KeyTab, '\r', '\n'},
+			data:  "Item 2",
+		},
+	} {
+		var in bytes.Buffer
+		var out bytes.Buffer
 
-		testcases = append(testcases, Testcase{
-			model: *choose.NewWithStrings(items, choose.WithTheme(choose.ThemeArrow)),
-			view:  "\n❯ Item 1\n  Item 2\n  Item 3\n",
-			keys:  vTc.keys,
-			data:  vTc.data,
-		})
-		testcases = append(testcases, Testcase{
-			model: *choose.NewWithStrings(items, choose.WithTheme(choose.ThemeArrow), choose.WithHelp(true)),
-			view:  "\n❯ Item 1\n  Item 2\n  Item 3\n\n? toggle help • enter confirm • q quit",
-			keys:  vTc.keys,
-			data:  vTc.data,
-		})
-	}
+		in.Write(testcase.keys)
+		m, err := tea.NewProgram(testcase.model, tea.WithInput(&in), tea.WithOutput(&out)).Run()
+		require.Nil(t, err)
 
-	for _, hTc := range hTcs {
-		testcases = append(testcases, Testcase{
-			model: *choose.NewWithStrings(items,
-				choose.WithTheme(choose.ThemeLine),
-				choose.WithKeyMap(choose.HorizontalKeyMap)),
-			view: "Item 1 / Item 2 / Item 3\n",
-			keys: hTc.keys,
-			data: hTc.data,
-		})
-		testcases = append(testcases, Testcase{
-			model: *choose.NewWithStrings(items,
-				choose.WithTheme(choose.ThemeLine),
-				choose.WithHelp(true),
-				choose.WithKeyMap(choose.HorizontalKeyMap)),
-			view: "Item 1 / Item 2 / Item 3\n\n? toggle help • enter confirm • q quit",
-			keys: hTc.keys,
-			data: hTc.data,
-		})
-	}
-
-	return testcases
-}
-
-func TestModel(t *testing.T) {
-	for _, tc := range testcases() {
-		tm := tester.Exec(t,
-			tc.model,
-			tc.keys,
-			tc.view,
-		)
-
-		m, ok := tm.(choose.Model)
-		require.Equal(t, true, ok)
-
-		require.Equal(t, tc.data, m.Data(), "keys: %s", tc.keys)
-		require.Equal(t, tc.data, m.DataString(), "keys: %s", tc.keys)
-		require.Equal(t, true, m.Quitting())
-		require.Nil(t, m.Error())
-	}
-
-	for _, quitKey := range []byte{'q', tester.KeyEsc, tester.KeyCtrlC} {
-		tm := tester.Exec(t, choose.NewWithStrings([]string{}), []byte{quitKey}, "\n")
-		m, ok := tm.(choose.Model)
-		require.Equal(t, true, ok)
-		require.Equal(t, constants.ErrUserQuit, m.Error())
+		require.Equal(t, testcase.data, m.(choose.Model).Data())
 	}
 }
