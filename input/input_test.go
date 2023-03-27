0f10a03fdda10b6b915afac91b730f233b81e2fd
Keith Chu <cqroot@outlook.com>
2023-03-28 01:51:04 +0800

    test(input): appearance tests moved to TestThemes

--- input/input_test.go
+++ input/input_test.go
@@ -1,83 +1,121 @@
 package input_test
 
 import (
+	"bytes"
 	"testing"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/cqroot/prompt/constants"
 	"github.com/cqroot/prompt/input"
-	"github.com/cqroot/prompt/tester"
 	"github.com/stretchr/testify/require"
 )
 
-type Testcase struct {
-	model input.Model
-	keys  []byte
-	data  string
-	view  string
-}
-
-func testcases() []Testcase {
-	testcases := make([]Testcase, 0, 20)
-
+func TestChoose(t *testing.T) {
 	defaultVal := "default value"
 	val := `abcdefghijklmnopqrstuvwxyz1.2.3.4.5.6.7.8.9.0-=~!@#$%^&*()_+[]\{}|;':",./<>?`
 
-	testcases = append(testcases, Testcase{
-		model: *input.New(defaultVal),
-		view:  "\x1b[7md\x1b[0mefault value",
-		keys:  []byte("\r\n"),
-		data:  defaultVal,
-	})
-	testcases = append(testcases, Testcase{
-		model: *input.New(defaultVal),
-		view:  "\x1b[7md\x1b[0mefault value",
-		keys:  []byte(val + "\r\n"),
-		data:  val,
-	})
-	testcases = append(testcases, Testcase{
-		model: *input.New(defaultVal, input.WithHelp(true)),
-		view:  "\x1b[7md\x1b[0mefault value\n\nenter confirm • esc quit",
-		keys:  []byte(val + "\r\n"),
-		data:  val,
-	})
-	testcases = append(testcases, Testcase{
-		model: *input.New(defaultVal, input.WithInputMode(input.InputInteger)),
-		view:  "\x1b[7md\x1b[0mefault value",
-		keys:  []byte(val + "\r\n"),
-		data:  "1234567890",
-	})
-	testcases = append(testcases, Testcase{
-		model: *input.New(defaultVal, input.WithInputMode(input.InputNumber)),
-		view:  "\x1b[7md\x1b[0mefault value",
-		keys:  []byte(val + "\r\n"),
-		data:  "1.234567890",
-	})
+	for _, testcase := range []struct {
+		model input.Model
+		keys  []byte
+		data  string
+	}{
+		{
+			model: *input.New(defaultVal),
+			keys:  []byte("\r\n"),
+			data:  defaultVal,
+		},
+		{
+			model: *input.New(defaultVal),
+			keys:  []byte(val + "\r\n"),
+			data:  val,
+		},
+		{
+			model: *input.New(defaultVal, input.WithInputMode(input.InputInteger)),
+			keys:  []byte(val + "\r\n"),
+			data:  "1234567890",
+		},
+		{
+			model: *input.New(defaultVal, input.WithInputMode(input.InputNumber)),
+			keys:  []byte(val + "\r\n"),
+			data:  "1.234567890",
+		},
+	} {
+		var in bytes.Buffer
+		var out bytes.Buffer
 
-	return testcases
+		in.Write(testcase.keys)
+		tm, err := tea.NewProgram(testcase.model, tea.WithInput(&in), tea.WithOutput(&out)).Run()
+		require.Nil(t, err)
+
+		m, ok := tm.(input.Model)
+		require.Equal(t, true, ok)
+
+		require.Equal(t, testcase.data, m.Data())
+		require.Equal(t, testcase.data, m.DataString())
+		require.Equal(t, true, m.Quitting())
+	}
 }
 
-func TestModel(t *testing.T) {
-	for _, tc := range testcases() {
-		tm := tester.Exec(t,
-			tc.model,
-			tc.keys,
-			tc.view,
-		)
+func TestErrors(t *testing.T) {
+	var in bytes.Buffer
+	var out bytes.Buffer
 
-		m, ok := tm.(input.Model)
-		require.Equal(t, true, ok)
+	in.Write([]byte{byte(tea.KeyCtrlC)})
+	tm, err := tea.NewProgram(*input.New(""), tea.WithInput(&in), tea.WithOutput(&out)).Run()
+	require.Nil(t, err)
 
-		require.Equal(t, tc.data, m.Data(), "keys: %s", tc.keys)
-		require.Equal(t, tc.data, m.DataString(), "keys: %s", tc.keys)
-		require.Equal(t, true, m.Quitting())
-		require.Nil(t, m.Error())
-	}
+	m, ok := tm.(input.Model)
+	require.Equal(t, true, ok)
 
-	for _, quitKey := range []byte{byte(tea.KeyEsc), byte(tea.KeyCtrlC)} {
-		tm := tester.Exec(t, input.New(""), []byte{quitKey}, "\x1b[7m \x1b[0m                                        ")
-		m, ok := tm.(input.Model)
-		require.Equal(t, true, ok)
-		require.Equal(t, constants.ErrUserQuit, m.Error())
+	require.Equal(t, constants.ErrUserQuit, m.Error())
+}
+
+func TestThemes(t *testing.T) {
+	defaultVal := "default value"
+
+	for _, testcase := range []struct {
+		model input.Model
+		view  string
+	}{
+		{
+			model: *input.New(defaultVal),
+			view:  "\x1b[7md\x1b[0mefault value",
+		},
+		{
+			model: *input.New(defaultVal, input.WithHelp(true)),
+			view:  "\x1b[7md\x1b[0mefault value\n\nenter confirm • esc quit",
+		},
+		{
+			model: func() input.Model {
+				var tm tea.Model
+				tm = input.New(defaultVal)
+				tm, _ = tm.Update(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("test")})
+				tm, _ = tm.Update(tea.KeyMsg{Type: tea.KeyEnter})
+				return tm.(input.Model)
+			}(),
+			view: "test\x1b[7m \x1b[0m                                    ",
+		},
+		{
+			model: func() input.Model {
+				var tm tea.Model
+				tm = input.New(defaultVal, input.WithEchoMode(input.EchoNone))
+				tm, _ = tm.Update(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("test")})
+				tm, _ = tm.Update(tea.KeyMsg{Type: tea.KeyEnter})
+				return tm.(input.Model)
+			}(),
+			view: "\x1b[7m \x1b[0m                                    ",
+		},
+		{
+			model: func() input.Model {
+				var tm tea.Model
+				tm = input.New(defaultVal, input.WithEchoMode(input.EchoPassword))
+				tm, _ = tm.Update(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("test")})
+				tm, _ = tm.Update(tea.KeyMsg{Type: tea.KeyEnter})
+				return tm.(input.Model)
+			}(),
+			view: "****\x1b[7m \x1b[0m                                    ",
+		},
+	} {
+		require.Equal(t, testcase.view, testcase.model.View())
 	}
 }
